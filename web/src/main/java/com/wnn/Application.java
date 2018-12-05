d3c656eda04b78c3bb6ff562846f2d65fe352ef2
wnn <784832001@qq.com>
2018-12-05 11:20:38 +0800

    项目分模块

--- web/src/main/java/com/wnn/Application.java
+++ web/src/main/java/com/wnn/Application.java
@@ -3,14 +3,12 @@ package com.wnn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author wnn
  * @date 2018/11/30 11:36
  */
 @SpringBootApplication
-@RestController
 public class Application {
 
     @RequestMapping("/")
